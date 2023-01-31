b8bfea8629bd315f7c8cace9f65e16a22629b6f5
rio <sw_park0205@naver.com>
2023-02-01 00:31:59 +0900

    Initial commit

---
+++ src/main/java/com/feb/arioboard/ArioboardApplication.java
@@ -0,0 +1,13 @@
+package com.feb.arioboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArioboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArioboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feb/arioboard/ArioboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feb.arioboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArioboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
